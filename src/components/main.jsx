43fbb81173b97519aae23028687ce84bf2cc1d51
Greenmort <greenmort@gmail.com>
2017-12-07 15:25:59 +0300

    babelrc edired

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/searchBar'
 import '../styles/main.scss';
 
 class Main extends Component {
@@ -6,7 +7,7 @@ class Main extends Component {
   render() {
     return (
       <div className="main">
-        <h1>Hello, React</h1>
+        <SearchBar/>
       </div>
     );
   }
